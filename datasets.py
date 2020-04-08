fe8b326a527ebedf77e27742f81e5b9fc1c984cd
Victor Mafra <32077156+MafraV@users.noreply.github.com>
2020-04-08 15:28:59 -0300

    Código para a separação das imagens em sequencias

--- datasets.py
+++ datasets.py
@@ -3,10 +3,12 @@ import cv2
 import csv
 import os
 
+#Recebendo todos os caminhos q terminem em .png
 images1 = glob.glob("C:/episodes/episode_1 - 1/*.png")
 images2 = glob.glob("C:/episodes/episode_1 - 2/*.png")
 images3 = glob.glob("C:/episodes/episode_1 - 3/*.png")
 
+#Criando uma lista para as imagens e outra para seus respectivos nomes
 episode = [cv2.imread(img) for img in images1+images2+images3] 
 names = [img for img in images1+images2+images3]
 states = []
@@ -15,20 +17,20 @@ size = len(episode)
 j=0
 k=1
 while (j<=size-10):
-    path = 'C:/videos/Episode1/Ep1 - '+str(k)
-    os.mkdir(path)
+    path = 'C:/videos/Episode1/Ep1 - '+str(k) #criando o nome da pasta
+    os.mkdir(path) #criando a pasta
     y=j+10
-    for i in range(j,y):
+    for i in range(j,y): #salvando as fotos na pasta criada
         string = os.path.basename(names[i])
         cv2.imwrite(path+'/'+string,episode[i])
-    name = names[y-1]
+    name = names[y-1] #salvando o estado da mão referente a decima foto
     if (name[-5]=='n'): state = 'open'
     else: state = 'closed'
     states.append(state)
     j+=1
     k+=1
-    
-    
+
+#Criando o .csv com os estados da mão de cada sequencia    
 with open('C:/videos/Episode1/Ep1 - States.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Sequence Number", "Hand State"])
